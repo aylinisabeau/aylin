79e5bb78f272799dfc1b26f419a7f53db2426dc6
aylin <aylincastillo@gmail.com>
2021-04-08 19:44:38 -0500

    Rspec category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,9 @@
 class Category < ApplicationRecord
   enum active_status: [:inactive, :active]
+  has_many :products
+
+  validates :name, presence: true, uniqueness: true
+
 
   def something
     binding.pry
