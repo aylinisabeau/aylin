e8b2ec133d4a975779dbd919ca3aa0767b55aa89
aylin <aylincastillo@gmail.com>
2021-04-13 16:31:47 -0500

    Cholofile

---
+++ spec/request/categories_request_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+RSpec.describe "Categories", type: :request do
+  describe "GET /categories" do
+    before(:all) do
+      FactoryBot.create_list :category, 20
+      get categories_path
+      @response_data = JSON.parse(response.body)
+    end
+
+    it "should get all the categories" do
+      scoped_data = Category.all
+      expect(@response_data.map{ |d| d["id"] }).to match_array(scoped_data.map(&:id))
+      scoped_data = Category.all.limit(10)
+      expect(@response_data["data"].map{ |d| d["id"] }).to match_array(scoped_data.map(&:id))
+    end
+    it "should have :ok status" do
+      expect(response.status).to eq(200)
+    end
+
+    # include_examples "paginated list"
+    include_examples "paginated list"
+  end
+end
